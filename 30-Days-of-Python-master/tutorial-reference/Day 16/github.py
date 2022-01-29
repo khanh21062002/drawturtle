71bd20f48f821458d54abe09a4eea3c26d177649
khanh21062002 <haduykhanh2106@gmail.com>
2022-01-30 00:51:20 +0700

    cập nhật lại

--- 30-Days-of-Python-master/tutorial-reference/Day 16/github.py
+++ 30-Days-of-Python-master/tutorial-reference/Day 16/github.py
@@ -1,34 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-# Github credentials
+#  tài khoản Github
 username = "khanh21062002"
 password = "987654321A@a"
 
-# initialize the Chrome driver
+# khởi chạy trình điều khiển Chrome
 driver = webdriver.Chrome("chromedriver")
-# head to github login page
+# truy cập trang đăng nhập github
 driver.get("https://github.com/login")
-# find username/email field and send the username itself to the input field
+# tìm tên người dùng / email và gửi chính tên người dùng đó vào chỗ nhập
 driver.find_element_by_id("login_field").send_keys(username)
-# find password input field and insert password as well
+# tìm chỗ nhập mật khẩu và chèn cả mật khẩu
 driver.find_element_by_id("password").send_keys(password)
-# click login button
+# bấm nút đăng nhập
 driver.find_element_by_name("commit").click()
-# wait the ready state to be complete
+# đợi trạng thái sẵn sàng hoàn tất
 WebDriverWait(driver=driver, timeout=10).until(
     lambda x: x.execute_script("return document.readyState === 'complete'")
 )
 error_message = "Incorrect username or password."
-# get the errors (if there are)
+# nếu xảy ra lỗi 
 errors = driver.find_elements_by_class_name("flash-error")
 # print the errors optionally
 # for e in errors:
 #     print(e.text)
-# if we find that error message within errors, then login is failed
+# nếu tìm thấy thông báo lỗi đó trong các lỗi, thì đăng nhập không thành công
 if any(error_message in e.text for e in errors):
     print("[!] Login failed")
 else:
     print("[+] Login successful")
-# close the driver
+# đóng driver
 # driver.close()
